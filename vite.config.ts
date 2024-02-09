8d515004d1bcab8582d04a610f218a1a56030f1e
Venkatesh14a <venkatesh14a@gmail.com>
2024-02-09 12:42:12 +0530

    added required packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specify the output directory for production build
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   },
-})
+});
